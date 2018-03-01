02fd1e55f3dcb1e55b8d8fbdd10a1ed76bf3eb52
Diogo Cezar <diogo@diogocezar.com>
2018-03-01 10:43:22 -0300

    Novos Conteúdos

---
+++ estrutura-de-dados/A04 - Ordenação - Parte 1/A04_BUBBLE_SORT.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/**
+  * Exemplo de Implementação do Método de Ordenação Bolha
+  * Diogo Cezar <diogo@diogocezar.com>
+  */
+
+
+/**
+  * Ordenação pelo método bolha
+  */
+void bubbleSort(int n, int v[]){
+    int i;
+    int j;
+    int aux;
+    for(i=0; i<n; i++){
+        for(j=0; j<n-1; j++){
+            if(v[j] > v[j+1]){
+                aux = v[j];
+                v[j] = v[j+1];
+                v[j+1] = aux;
+            }
+        }
+    }
+}
+
+/**
+  * Impressão de um vetor
+  */
+void printVet(int n, int v[]){
+    int j;
+    for(j = 0; j<n ; j++){
+        printf("[%d] ", v[j]);
+    }
+    printf("\n");
+}
+
+/**
+  * Função Principal
+  */
+int main(){
+    int v[7] = {80,12,1,5,18,20,3};
+    printf("Vetor original:\n");
+    printVet(7, v);
+    bubbleSort(7, v);
+    printf("Vetor ordenado:\n");
+    printVet(7, v);
+}
---
+++ estrutura-de-dados/A04 - Ordenação - Parte 1/A04_SELECTION_SORT.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/**
+  * Exemplo de Implementação do Método de Ordenação Bolha
+  * Diogo Cezar <diogo@diogocezar.com>
+  */
+
+
+/**
+  * Ordenação pelo método bolha
+  */
+void selectionSort(int n, int v[]){
+    int i, j, min, aux;
+    for(i=0; i<n-1; i++){
+        min = i;
+        for(j=i+1; j<n; j++){
+            if(v[j] < v[min]){
+                min = j;
+            }
+        }
+        aux = v[i];
+        v[i] = v[min];
+        v[min] = aux;
+    }
+}
+
+/**
+  * Impressão de um vetor
+  */
+void printVet(int n, int v[]){
+    int j;
+    for(j = 0; j<n ; j++){
+        printf("[%d] ", v[j]);
+    }
+    printf("\n");
+}
+
+/**
+  * Função Principal
+  */
+int main(){
+    int v[7] = {80,12,1,5,18,20,3};
+    printf("Vetor original:\n");
+    printVet(7, v);
+    selectionSort(7, v);
+    printf("Vetor ordenado:\n");
+    printVet(7, v);
+}
