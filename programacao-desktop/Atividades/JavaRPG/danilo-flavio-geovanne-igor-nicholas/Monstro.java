1650a145887e196b4ae1c810f23a0585c3a9214d
Geovanne Moro <geovannemoro@hotmail.com>
2018-03-13 23:40:12 -0300

    segunda versao

--- programacao-desktop/Atividades/JavaRPG/danilo-flavio-geovanne-igor-nicholas/Monstro.java
+++ programacao-desktop/Atividades/JavaRPG/danilo-flavio-geovanne-igor-nicholas/Monstro.java
@@ -1,8 +1,11 @@
 class Monstro extends Criatura{
+    
     public Monstro(String nome, String classe, int nivel, int vida, int forca, int magia, int agilidade, int destreza, int inteligencia, int carisma){
+    {
         super(nome, classe, nivel, vida, forca, magia, agilidade, destreza, inteligencia, carisma);
-
     }
-
+        
+    public void descansar(){}
+  
     
 }
