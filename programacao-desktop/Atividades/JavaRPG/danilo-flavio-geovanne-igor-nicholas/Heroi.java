6641267f6cebbc6be1f1e7097dfdee049709625d
Geovanne Moro <geovannemoro@hotmail.com>
2018-03-13 23:45:52 -0300

    segunda versao

--- programacao-desktop/Atividades/JavaRPG/danilo-flavio-geovanne-igor-nicholas/Heroi.java
+++ programacao-desktop/Atividades/JavaRPG/danilo-flavio-geovanne-igor-nicholas/Heroi.java
@@ -2,12 +2,23 @@ class Heroi extends Criatura{
     
     int bencao;
     
-    public Heroi (String nome, String classe, int nivel, int vida, int forca, int magia, int agilidade, int destreza, int inteligencia, int carisma, int bencao){
+    public Heroi (String nome, String classe, int nivel, int vida, int forca, int magia, int agilidade, int destreza, int inteligencia, int carisma, int bencao)
+    {    
         super(nome, classe, nivel, vida, forca, magia, agilidade, destreza, inteligencia, carisma);
         this.bencao = bencao;
     }
-
+     
+    public void descansar()
+    {
+    }
+    
+    public void setBencao(int bencao)
+    {
+        this.bencao = bencao;
+    }
+    
     public int getBencao(){
         return bencao;
     }
+  
 }
