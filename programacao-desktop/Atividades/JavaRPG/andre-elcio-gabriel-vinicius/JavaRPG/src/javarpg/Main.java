f3f15a98e6dbf4b468545aa1cdc4eebdd347ea4e
Gabriel de Abreu <gabrieldeabreu.ti@gmail.com>
2018-03-14 18:25:17 -0300

    Recording  heroes in Main

--- programacao-desktop/Atividades/JavaRPG/andre-elcio-gabriel-vinicius/JavaRPG/src/javarpg/Main.java
+++ programacao-desktop/Atividades/JavaRPG/andre-elcio-gabriel-vinicius/JavaRPG/src/javarpg/Main.java
@@ -21,9 +21,14 @@ public class Main {
         Monster LordBlack = new Monster("Lord Black","Human",10,100,20,32,12,10,10,1);
         Monster Matilda = new Monster("Matilda","Elf",10,100,80,10,8,10,18,5);
         
-        Hero DarkGlorysson= new Hero("Dark Glorysson","Humano",8,100,100,10,5,10,15,2,10);
-        System.out.println(DarkGlorysson.__toString());
+        Hero DarkGlorysson= new Hero("Dark Glorysson","Human",8,100,100,10,5,10,15,2,10);
+        System.out.println(DarkGlorysson.__toString()+"\n");
         
+        Hero Mellayne= new Hero("Mellayne","Human",5,100,80,25,10,8,2,20,10);
+        System.out.println(Mellayne.__toString()+"\n");
+        
+        Hero Gryin= new Hero("Gryin", "Dwarf",10,100,50,30,5,10,5,1,10);
+        System.out.println(Gryin.__toString());
     }
     
 }
