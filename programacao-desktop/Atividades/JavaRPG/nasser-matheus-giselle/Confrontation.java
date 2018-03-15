c52f6dc6af64bcf4338a55a4d9cfe899945df311
Nasser Ruiz Rehman <nasser.ruizrehman@gmail.com>
2018-03-15 16:42:43 -0300

    Optimizations

--- programacao-desktop/Atividades/JavaRPG/nasser-matheus-giselle/Confrontation.java
+++ programacao-desktop/Atividades/JavaRPG/nasser-matheus-giselle/Confrontation.java
@@ -3,12 +3,22 @@ public class Confrontation
     public static void main(String[] args)
     {
 
-        Hero hero = new Hero("DARK GLORYSSON", "Humano", 8, 100, 100, 10, 5, 10, 15, 2, 10);
+        Hero hero = new Hero("DARK GLORYSSON", "Human", 8, 100, 100, 10, 5, 10, 15, 2, 10);
         hero.__toString();
+        try {
+            Thread.sleep(2500);
+        } catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
         System.out.println("");
-        Monster monster = new Monster("MONTARO", "Elfo das Trevas", 7, 100, 100, 10, 5, 10, 15, 10);
+        Monster monster = new Monster("MONTARO", "Dark Elf", 7, 100, 100, 10, 5, 10, 15, 10);
         monster.__toString();
-
+        try {
+            Thread.sleep(2500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         int flag = 1;
         int count_round=0;
         int attack;
@@ -25,46 +35,46 @@ public class Confrontation
                 attack = (int) monster.attack();
                 defence = (int) hero.defend();
 
-                System.out.println(monster.getName()+ " attacked ("+attack+"X"+defence+""+hero.getName());
-                System.out.println("");
+                System.out.println(monster.getName()+ " attacked ("+attack+"X"+defence+")"+hero.getName());
 
                 if (attack > defence)
                 {
                     hero.losesLife(8);
                     System.out.println(hero.getName() + " lost 8 of life!");                   
-                    System.out.println("Life of Hero: "+ hero.getLife());
-                    System.out.println("Life of Monster: " + monster.getLife());
-                    System.out.println("");
                 } else {
                     System.out.println(hero.getName() + " blocked the attack!");
-                    System.out.println("Life of Hero: " + hero.getLife());
-                    System.out.println("Life of Monster: " + monster.getLife());
-                    System.out.println("");
+                }
+                System.out.println("Life of Hero: " + hero.getLife());
+                System.out.println("Life of Monster: " + monster.getLife());
+                System.out.println("");
+                try {
+                    Thread.sleep(1500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             } else if (flag == 2)
             {
                 flag = 1;
                 attack = (int) hero.attack();
                 defence = (int) monster.defend();
-                System.out.println(hero.getName()+ " attacked ("+attack+"X"+defence+""+monster.getName());
-                System.out.println("");
+                System.out.println(hero.getName()+ " attacked ("+attack+"X"+defence+")"+monster.getName());
 
                 if (attack > defence)
                 {
                     monster.losesLife(8);
                     System.out.println(monster.getName() + "lost 8 of life!");
-                    System.out.println("Life of Hero: " + hero.getLife());
-                    System.out.println("Life of Monster: " + monster.getLife());
-                    System.out.println("");
-
                 } else {
                     System.out.println(monster.getName() + " blocked the attack!");
-                    System.out.println("Life of Hero: " + hero.getLife());
-                    System.out.println("Life of Monster: " + monster.getLife());
-                    System.out.println("");
+                }
+                System.out.println("Life of Hero: " + hero.getLife());
+                System.out.println("Life of Monster: " + monster.getLife());
+                System.out.println("");
+                try {
+                    Thread.sleep(1500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
-
         }
     }
 }
