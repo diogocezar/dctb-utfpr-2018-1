e3d1975826b69b2ae29f929493c9b3a4044dfa2b
Geovanne Moro <geovannemoro@hotmail.com>
2018-03-15 20:46:02 -0300

    Update Criatura.java

--- programacao-desktop/Atividades/JavaRPG/danilo-flavio-geovanne-igor-nicholas/Criatura.java
+++ programacao-desktop/Atividades/JavaRPG/danilo-flavio-geovanne-igor-nicholas/Criatura.java
@@ -2,10 +2,10 @@ import java.util.Random;
 class Criatura{
 
     private String nome, classe;
-    private int nivel, vida, forca, magia, agilidade, destreza, inteligencia, carisma;
+    private float nivel, vida, forca, magia, agilidade, destreza, inteligencia, carisma;
     //private double vida;
 
-    Criatura(String nome, String classe, int nivel, int vida, int forca, int magia, int agilidade, int destreza, int inteligencia, int carisma){
+    Criatura(String nome, String classe, float nivel, float vida, float forca, float magia, float agilidade, float destreza, float inteligencia, float carisma){
         this.nome = nome;
         this.classe = classe;
         this.nivel = nivel;
@@ -26,35 +26,35 @@ class Criatura{
         return classe;
     }
 
-    public int getNivel(){
+    public float getNivel(){
         return nivel;
     }
 
-    public int getVida(){
+    public float getVida(){
         return vida;
     }
 
-    public int getForca(){
+    public float getForca(){
         return forca;
     }
 
-    public int getMagia(){
+    public float getMagia(){
         return magia;
     }
 
-    public int getAgilidade(){
+    public float getAgilidade(){
         return agilidade;
     }
 
-    public int getDestreza(){
+    public float getDestreza(){
         return destreza;
     }
 
-    public int getInteligencia(){
+    public float getInteligencia(){
         return inteligencia;
     }
 
-    public int getCarisma(){
+    public float getCarisma(){
         return carisma;
     }
  
@@ -65,14 +65,14 @@ class Criatura{
             return (double)nivel*forca*(double)(0.75*agilidade+0.25*destreza)/100;
         else
             return (double)nivel*inteligencia*(magia+agilidade)/435;*/
-        if(classe=="Humano")
-            return (gerador.nextInt(2))*nivel*(7*forca+3*agilidade+3*destreza)/10;
-        else
-            return (gerador.nextInt(2))*nivel*(7*inteligencia+2*magia+agilidade)/10;
+       // if(classe=="Humano")
+      //      return (gerador.nextInt(2))*nivel*(7*forca+3*agilidade+3*destreza)/10;
+      //  else
+       //     return (gerador.nextInt(2))*nivel*(7*inteligencia+2*magia+agilidade)/10;
     }
-    int defender(){ // função defender() retorna o total de dano defendido
+    public int defender(){ // função defender() retorna o total de dano defendido
        // return nivel*(double)(0.4*destreza+0.3*carisma+0.3*inteligencia)/(double)(vida/3);
-       return (gerador.nextInt(2))*this.vida/8*(5*destreza+4*agilidade+carisma)/10;
+      // return (gerador.nextInt(2))*this.vida/8*(5*destreza+4*agilidade+carisma)/10;
     }
 
     void vivo(){
@@ -86,15 +86,15 @@ class Criatura{
         if(ataque>=defesa)
             this.vida -= ataque-defesa;
         int toggleRegen = gerador.nextInt(3);
-        if (toggleRegen==0) revitalizar(); // 1/3 de chance de ativar a skill revitalizar
+        //if (toggleRegen==0) revitalizar(); // 1/3 de chance de ativar a skill revitalizar
     }
 
     void revitalizar(){
-        int regeneracao = gerador.nextInt(65);
-        if (this.vida+regeneracao>100) this.vida=100; // regeneraçao do HP entre cada ataque sofrido
-        else this.vida+= regeneracao;
+        //int regeneracao = gerador.nextInt(65);
+        //if (this.vida+regeneracao>100) this.vida=100; // regeneraçao do HP entre cada ataque sofrido
+       // else this.vida+= regeneracao;
 
-        System.out.println("Revitalizar disparado. Regenerados "+regeneracao+" pontos de vida.");
+       // System.out.println("Revitalizar disparado. Regenerados "+regeneracao+" pontos de vida.");
     }
     void descansar(){
         
