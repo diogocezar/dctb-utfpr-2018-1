735a4a323f520a613acb3ae461b93ea667140e2b
Nasser Ruiz Rehman <nasser.ruizrehman@gmail.com>
2018-03-22 10:26:11 -0300

    Alteração final

--- programacao-desktop/Atividades/JavaRPG/nasser-matheus-giselle/Confrontation.java
+++ programacao-desktop/Atividades/JavaRPG/nasser-matheus-giselle/Confrontation.java
@@ -1,33 +1,69 @@
+import java.util.ArrayList;
+
 public class Confrontation 
 {
     public static void main(String[] args)
     {
-
         Hero hero = new Hero("DARK GLORYSSON", "Human", 8, 100, 100, 10, 5, 10, 15, 2, 10);
-        hero.__toString();
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e)
-        {
-            e.printStackTrace();
-        }
-        System.out.println("");
+        Hero hero2 = new Hero("MELLAYNE", "Human", 5, 100, 80, 25, 10, 8, 2, 20, 10);
+        Hero hero3 = new Hero("GRYIN", "Dwarf", 10, 100, 50, 30, 5, 10, 5, 1, 10);
+
+
         Monster monster = new Monster("MONTARO", "Dark Elf", 7, 100, 100, 10, 5, 10, 15, 10);
-        monster.__toString();
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        Monster monster2 = new Monster("LORD BLACK", "Human", 10, 100, 20, 32, 12, 10, 10, 1);
+        Monster monster3 = new Monster("MATILDA", "Elf", 10, 100, 80, 10, 8, 10, 18, 5);
+        
+
+        ArrayList<Hero> first_hero = new ArrayList<Hero>();
+        ArrayList<Monster> first_monster = new ArrayList<Monster>();
+        first_hero.add(hero);
+        first_hero.add(hero2);
+        first_hero.add(hero3);
+        first_monster.add(monster);
+        first_monster.add(monster3);
+        first_monster.add(monster2);
+         
+        ArrayList<Creature> second_fight = new ArrayList<>();
+        //Round 1
+        int aux = 0;
+        while (aux != 3){
+           second_fight.add(battle(first_hero.get(aux), first_monster.get(aux)));
+            aux++;
         }
+        
+        Creature final_fighter_1 = battle(second_fight.get(0), second_fight.get(1));
+        Creature final_fighter_2 = second_fight.get(2);
+
+        Creature winner = battle(final_fighter_1, final_fighter_2);
+
+
+        System.out.println("The winner of the championship is " + winner.getName());
+
+
+    }
+
+        public static Creature battle(Creature hero, Creature monster){
+
+
         int flag = 1;
         int count_round=0;
         int attack;
         int defence;
+        hero.__toString();
+        System.out.println(" VS ");
+        monster.__toString();        
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         while (hero.isAlive() && monster.isAlive())
         {
             attack = 0;
             defence = 0;
             count_round++;
+            System.out.println("");
             System.out.println("ROUND "+count_round);
             if (flag == 1)
             {
@@ -47,11 +83,7 @@ public class Confrontation
                 System.out.println("Life of Hero: " + hero.getLife());
                 System.out.println("Life of Monster: " + monster.getLife());
                 System.out.println("");
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+
             } else if (flag == 2)
             {
                 flag = 1;
@@ -69,12 +101,43 @@ public class Confrontation
                 System.out.println("Life of Hero: " + hero.getLife());
                 System.out.println("Life of Monster: " + monster.getLife());
                 System.out.println("");
+            }
+
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+
+            if (!hero.isAlive()){
+                System.out.println("");
+                System.out.println(monster.getName() + " WINS THE BATTLE!");
+                System.out.println("");
+                monster.rest();
                 try {
-                    Thread.sleep(500);
+                    Thread.sleep(1200);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+                
+                return (Creature) monster;
             }
+            else if (!monster.isAlive()) {
+                System.out.println(hero.getName() + " WINS THE BATTLE!");
+                System.out.println("");
+                hero.rest(); 
+                try {
+                    Thread.sleep(1200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                
+                return (Creature) hero;
+                
+            }
+            
         }
+        return null;
     }
 }
