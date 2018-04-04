68c7f236849b0f2783f8b006313151075a3346ac
Diogo Cezar <diogo@diogocezar.com>
2018-04-04 10:15:31 -0300

    Pastas e Exemplos

--- programacao-web/A08 - Live code JavaScript/js/app.js
+++ programacao-web/A08 - Live code JavaScript/js/app.js
@@ -11,13 +11,33 @@
  * Implementar um exemplo simples de função
  */
 
+function sum(number){
+    return number + 2;
+}
+
+console.log(sum(4));
+
 /**
  * Implementar uma função, atribuíla a uma variável e executar
  */
 
+ var s = function (number){
+    return ++number;
+ }
+
+console.log(s(4));
+
 /**
  * Implementar um exemplo de passagem de função como callback
  */
+
+function exCall(f){
+    f(5);
+}
+
+exCall(function(val){
+    console.log(val+10);
+})
 
 /**
  * VARIÁVEIS
@@ -27,6 +47,11 @@
  * Implementar exemplos de variáveis
  */
 
+ var a = 19;
+ var b = "Teste";
+ var c = [1,2,3,4];
+ var d = {'name' : 'diogo'};
+
 /**
  * ARRAYS
  */
@@ -34,6 +59,8 @@
 /**
  * Implementar exemplos de arrays
  */
+
+var str = ['farah', 'judit', 'jessé', 'irley'];
 
 /**
  * LAÇOS
@@ -43,9 +70,16 @@
  * Implementar exemplo de forEach com arrays (utilizando CB)
  */
 
+str.forEach(function(v){
+    console.log(v);
+});
+
 /**
  * Implementar função equivalente sem utilização  do forEach
  */
+
+ for(var i=0; i<str.length; i++)
+    console.log(str[i]);
 
 /**
  * CONDIÇÕES
@@ -55,6 +89,15 @@
  * Implementar exemplos de utilização de == e ===
  */
 
+var a = 1;
+var b = '1';
+
+//debugger;
+
+if(a = b) console.log('ok');
+if(a == b) console.log('ok2');
+if(a === b) console.log('ok3');
+
 /**
  * OBJETO LITERAL
  */
@@ -62,6 +105,16 @@
 /**
  * Implementar a utilização de um Objeto Literal
  */
+
+const ol = {
+    newTitle : "Olá novo título",
+    init: function(){
+        var el = document.querySelector("#title-1");
+        el.innerHTML = this.newTitle;
+    }
+}
+
+ol.init();
 
 /**
  * OBJETO CONSTRUTOR
@@ -71,6 +124,20 @@
  * Implementar a utilização de um Objeto Construtor
  */
 
+function Fruits(color, shape){
+    this.color = color;
+    this.shape = shape;
+    this.print = function(){
+        console.log(this.color + ' - ' + this.shape);
+    }
+}
+
+var apple = new Fruits('red', 'round');
+
+apple.print();
+
+
+
 /**
  * ARRAYS DE OBJETOS
  */
@@ -78,6 +145,21 @@
 /**
  * Implementar a utilização arrays de objetos
  */
+
+var a = [
+    {
+        name : 'aaa'
+    },
+    {
+        name : 'bbb',
+        age : 12
+    },
+    {
+        name: 'ccc'
+    },
+];
+
+console.log(a[1]);
 
 /**
  * INTERAÇÕES COM HTML
@@ -103,6 +185,9 @@
  * Implementar a utilização style
  */
 
+var title = document.querySelector("#title-1");
+title.style.color = 'blue';
+
 /**
  * EVENTOS
  */
@@ -111,6 +196,8 @@
  * Implementar addEventListner
  */
 
+
+
 /**
  * FORMS
  */
@@ -118,3 +205,26 @@
 /**
  * Implementar uma validação simples de formulário
  */
+
+const validation = {
+    firstName : null,
+    lastName : null,
+    validate: function(){
+        validation.firstName = document.querySelector("#firstName");
+        validation.lastName = document.querySelector("#lastName");
+        validation.firstName.style.borderColor = 'black';
+        validation.lastName.style.borderColor = 'black';
+        if (validation.firstName.value != '' && validation.lastName.value != '')
+            alert('Enviado com sucesso!');
+        else{
+            validation.firstName.style.borderColor = 'red';
+            validation.lastName.style.borderColor = 'red';
+        }
+    },
+    init: function(){
+        var btn = document.querySelector("#send");
+        btn.addEventListener('click', validation.validate);
+    }
+}
+
+validation.init();
