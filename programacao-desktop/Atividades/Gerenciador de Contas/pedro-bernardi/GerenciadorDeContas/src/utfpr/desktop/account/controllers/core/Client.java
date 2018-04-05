cd4897cf64a8a0f8bba470c6c36011402eab409b
Pedro Bernardi Alves <pedrobealves@gmail.com>
2018-04-05 15:18:24 -0300

    Entrega da atividade de gerenciamento de contas

---
+++ programacao-desktop/Atividades/Gerenciador de Contas/pedro-bernardi/GerenciadorDeContas/src/utfpr/desktop/account/controllers/core/Client.java
@@ -0,0 +1,63 @@
+package utfpr.desktop.account.controllers.core;
+
+import java.util.Date;
+
+public class Client {
+    private int id;
+    private static int count = 0;
+    private double balance;
+    private String agency;
+    private String holder;
+    private Date dateCreated;
+
+    public Client(double balance, String agency, String holder) {
+        this.id = ++count;
+        this.balance = balance;
+        this.agency = agency;
+        this.holder = holder;
+        dateCreated = new Date();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getAgency() {
+        return agency;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setAgency(String agency) {
+        this.agency = agency;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", balance=" + balance +
+                ", agency='" + agency + '\'' +
+                ", holder='" + holder + '\'' +
+                ", dateCreated=" + dateCreated +
+                '}';
+    }
+}
