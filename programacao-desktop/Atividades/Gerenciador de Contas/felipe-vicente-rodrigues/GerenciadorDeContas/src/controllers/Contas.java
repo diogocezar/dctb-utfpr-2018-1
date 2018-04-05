9235877e3785f0984945806ef2a1b561375acf4d
rodriguesfelipe <feliperodriguesxz@gmail.com>
2018-04-04 21:58:28 -0300

    Atividade versão 1.

---
+++ programacao-desktop/Atividades/Gerenciador de Contas/felipe-vicente-rodrigues/GerenciadorDeContas/src/controllers/Contas.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+/**
+ *
+ * @author felipe
+ */
+public class Contas {
+
+    String titular;
+    String agencia;
+    int numero;
+    double saldo;
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+}
