52e195930fa73523911193128e3da45895eef635
JoaoPirolo <jc_pirolo@hotmail.com>
2018-04-05 11:21:09 -0300

    Calculadora

--- estrutura-de-dados/Materiais/README.md
+++
@@ -1,9 +0,0 @@
-# Aqui estão alguns links interessantes
-
-* http://www.inf.ufpr.br/cursos/ci067/Docs/NotasAula/
-
-* https://www.ime.usp.br/~pf/algoritmos/
-
-* http://math.hws.edu/eck/js/sorting/xSortLab.html
-
-* http://www.danilogiacobo.eti.br/
--- programacao-desktop/Atividades/JavaRPG/matheus-evair-gabriel/src/utils/Hero.java
+++
@@ -1,9 +0,0 @@
-package utils;
-
-public class Hero extends Creature{
-
-    public Hero(String name, String classType, double level, double life, double magic, double power, double agility, double dexterity, double intelligence, double charisma, Integer blessing) {
-        super(name, classType, level, life, magic, power, agility, dexterity, intelligence, charisma, blessing);
-    }
-    
-}
