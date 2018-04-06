823e70e30359f545b33eb93b6178bf949de00827
YagoMilitao <yagolano@hotmail.com>
2018-04-05 23:31:21 -0300

    envio

---
+++ programacao-desktop/Atividades/Gerenciador de Contas/yagomilitao/yagomilitao/principal/Conta.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+/**
+ *
+ * @author kamil
+ */
+public class Conta {
+    
+    String titularConta;
+    String agenciaConta;
+    int numeroConta;
+    double saldoConta;
+
+    public String getTitularConta() {
+        return titularConta;
+    }
+
+    public void setTitularConta(String titularConta) {
+        this.titularConta = titularConta;
+    }
+
+    public String getAgenciaConta() {
+        return agenciaConta;
+    }
+
+    public void setAgenciaConta(String agenciaConta) {
+        this.agenciaConta = agenciaConta;
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public double getSaldoConta() {
+        return saldoConta;
+    }
+
+    public void setSaldoConta(double saldoConta) {
+        this.saldoConta = saldoConta;
+    }
+
+    public String getAgencia() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+   
+    
+    
+}
