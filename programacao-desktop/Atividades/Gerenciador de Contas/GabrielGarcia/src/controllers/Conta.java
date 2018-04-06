e36faf1bb127f2c9e14cb78d648ae50a98bb6d05
Gabriel Garcia <31714430+gabriel-hg58@users.noreply.github.com>
2018-04-05 21:33:39 -0300

    Add files via upload

---
+++ programacao-desktop/Atividades/Gerenciador de Contas/GabrielGarcia/src/controllers/Conta.java
@@ -0,0 +1,41 @@
+package controllers;
+
+public class Conta {
+
+    private String titular;
+    private String agencia;
+    private double saldo;
+    private int numero;
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }    
+}
