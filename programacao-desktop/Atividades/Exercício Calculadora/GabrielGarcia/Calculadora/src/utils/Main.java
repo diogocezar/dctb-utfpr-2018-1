0d49d31d63f7666a40ce36f9fbe3833ba205f01d
Gabriel Garcia <31714430+gabriel-hg58@users.noreply.github.com>
2018-04-09 22:47:00 -0300

    GabrielGarcia

---
+++ programacao-desktop/Atividades/Exercício Calculadora/GabrielGarcia/Calculadora/src/utils/Main.java
@@ -0,0 +1,12 @@
+package utils;
+
+import view.Calculator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        calc.setVisible(true);
+    }
+    
+}
