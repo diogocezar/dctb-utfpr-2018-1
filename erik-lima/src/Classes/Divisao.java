5016d271ddea17306496ce46f04d2fe9d5366e1f
Erik Lima Floro Silva <37297055+ErikLimaUtfpr@users.noreply.github.com>
2018-04-12 21:17:55 -0300

    exercicio de calculadora

---
+++ erik-lima/src/Classes/Divisao.java
@@ -0,0 +1,15 @@
+
+package Classes;
+
+/**
+ *
+ * @author Erik Lima
+ */
+public class Divisao {
+    public double div(double num1, double num2){
+        
+        return num1 / num2;
+        
+    }
+    
+}
---
+++ erik-lima/src/Classes/Soma.java
@@ -0,0 +1,15 @@
+
+package Classes;
+
+/**
+ *
+ * @author Erik Lima
+ */
+public class Soma {
+    
+    public double som(double num1, double num2){
+        return num1 + num2;
+        
+    }
+    
+}
