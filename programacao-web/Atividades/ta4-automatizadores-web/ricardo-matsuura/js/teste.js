c25fd84a1d71a5d333e1acd17a78ae840001c39b
Ricardo Matsuura <rymats@hotmail.com>
2018-04-16 02:33:11 -0300

    automatizadores

---
+++ programacao-web/Atividades/ta4-automatizadores-web/ricardo-matsuura/js/teste.js
@@ -0,0 +1,14 @@
+function soma(a,b) {
+	return a+b;
+}
+
+function fibonacci(num) {
+	var i = 0, j = 1, cont = 0;
+	while(cont < num) {
+		console.log("i");
+		var aux = i;
+	}
+	i = j;
+	j += aux;
+	cont++;
+}
