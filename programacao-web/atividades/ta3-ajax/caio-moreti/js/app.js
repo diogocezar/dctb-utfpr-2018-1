0b5b4fd3a4e55c6b5e0dcda374a2f9f4ff4ae598
CaioMoreti <caio_pertile@hotmail.com>
2018-04-15 22:33:53 -0300

    Atividades TA3 e TA4

---
+++ programacao-web/atividades/ta3-ajax/caio-moreti/js/app.js
@@ -0,0 +1,57 @@
+
+//Função baseada em um exemplo da internet
+function json(){
+	var qtd;
+	var retorno;
+
+	// Resgatar valores.
+	//Criando protótipo da função para um objeto acessa-la
+	json.prototype.resgatarValores = function(){
+		$('#resultado').html('Carregando dados...');
+
+
+		// Estrutura de resultado.
+		$.getJSON('./json/database.json', function(data){
+
+			this.qtd = data.products.length;
+			this.retorno = '';
+
+			this.retorno += data.configs.title;
+			this.retorno += '</br>'+data.configs.subTitle;
+			this.retorno += '</br></br>Email: '+data.configs.email;
+			
+			
+			
+			this.retorno += '</br></br>'+data.configs.url;
+			this.retorno += '</br></br>'+'Telefone para contato: '+data.configs.phone;
+			this.retorno += '</br></br>'+data.configs.description[0].p;
+
+
+
+			$('#info').html(this.retorno);
+
+
+			//Resetando a string retorno, para percorrer os produtos
+			this.retorno = '';
+
+			for (i = 0; i < this.qtd; i++){
+				this.retorno += 'Produto: ' + data.products[i].name + '<br/>';
+				this.retorno += '<img class="resolucao" src="' + data.products[i].image + '" class="img-decorative" alt="Produto"/>'+'<br/>';
+				for(j=0;j<data.products[i].description.length;j++){
+					this.retorno += 'Descrição: ' + data.products[i].description[j].p + '<br/>';
+				}
+
+				this.retorno += 'Preço: R$' + data.products[i].price + ',00' + '<br/><br/>';
+			}
+				
+
+			$('#resultado').html(this.retorno);
+		});
+
+	}
+
+}
+
+// Objeto.
+var obj = new json();
+obj.resgatarValores();
