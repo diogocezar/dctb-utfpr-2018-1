4e7c176d71e9feed8e72fecb381eb1d5d6fc9d8a
Diogo Cezar <diogo@diogocezar.com>
2018-04-17 15:58:16 -0300

    Exemplo Pilha

---
+++ estrutura-de-dados/a13-pilhas/A13_PILHAS.c
@@ -0,0 +1,103 @@
+/**
+* Professor Diogo Cezar Teixeira Batista
+* <diogo@diogocezar.com>
+* <diogoc@utfpr.edu.br>
+* Exemplo de Pilha
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node{
+	int value;
+	struct Node * next;
+} Node;
+
+typedef struct Stack{
+	Node * top;
+} Stack;
+
+Stack * create(){
+	Stack * q = (Stack*) malloc(sizeof(Stack));
+	q->top = NULL;
+	return q;
+}
+
+void push(Stack * s, int value){
+	Node * new = (Node*) malloc(sizeof(Node));
+	new->value = value;
+	new->next = s->top;
+	s->top = new;
+}
+
+int isEmptyStack(Stack * s){
+    return(s->top == NULL);
+}
+
+void display(Stack * s){
+    Node * p = s->top;
+    if(isEmptyStack(s)){
+        printf("A pilha está vazia!\n");
+    }
+    else{
+        while(p != NULL){
+            printf("%d\n", p->value);
+            p = p->next;
+        }
+    }
+}
+
+Node * search(Stack * s, int value){
+    Node * p;
+    for(p=s->top; p!=NULL; p=p->next){
+        if(p->value == value)
+            return p;
+    }
+    return NULL;
+}
+
+void freeStack(Stack * s){
+	Node * p = s->top;
+	while(p != NULL){
+		Node * t = p->next;
+		free(p);
+		p = t;
+    }
+    free(p);
+}
+
+int pop(Stack * s){
+	Node * t;
+	int v;
+	if(isEmptyStack(s)){
+		printf("\nPilha já vazia!\n");
+		return 0;
+	}
+    t = s->top;
+    v = t->value;
+    s->top = t->next;
+	free(t);
+	return v;
+}
+int main(){
+    Stack * s;
+    s = create();
+    Node * searched = NULL;
+    printf("A pilha está vazia (0,1) : %d\n", isEmptyStack(s));
+    display(s);
+    push(s, 10);
+    push(s, 20);
+    push(s, 30);
+    printf("A pilha está vazia (0,1) : %d\n", isEmptyStack(s));
+    display(s);
+    printf("Buscando elemento:\n");
+	searched = search(s, 10);
+	if(searched != NULL){
+		printf("Encontrou o elemento: %d\n", searched->value);
+	}
+    display(s);
+    printf("Removendo elemento: %d\n", pop(s));
+    printf("Liberando a pilha.\n");
+    freeStack(s);
+    return 0;
+}
